fce606cf0c48d0782e7b659022ac37d26ebc5903
Alex Kaminsky <akaminsky@azavea.com>
2019-01-14 22:19:58 -0500

    Add skeleton mamlValidator server

---
+++ maml-validator/src/main/scala/com/example/mamlvalidator/MamlValidatorServer.scala
@@ -0,0 +1,25 @@
+package com.example.mamlvalidator
+
+import cats.effect.{Effect, IO}
+import fs2.StreamApp
+import org.http4s.server.blaze.BlazeBuilder
+
+import scala.concurrent.ExecutionContext
+
+object MamlValidatorServer extends StreamApp[IO] {
+  import scala.concurrent.ExecutionContext.Implicits.global
+
+  def stream(args: List[String], requestShutdown: IO[Unit]) =
+    ServerStream.stream[IO]
+}
+
+object ServerStream {
+
+  def mamlValidatorService[F[_]: Effect] = new MamlValidatorService[F].service
+
+  def stream[F[_]: Effect](implicit ec: ExecutionContext) =
+    BlazeBuilder[F]
+      .bindHttp(8080, "0.0.0.0")
+      .mountService(mamlValidatorService, "/")
+      .serve
+}
